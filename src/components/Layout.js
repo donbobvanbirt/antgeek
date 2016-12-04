08fbf891dde1f441fb3e8708fa8b628dcaeccad8
Donovan <donovan6925@gmail.com>
2016-12-04 16:15:09 -0500

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Layout extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>AntGeek</h1>
+      </div>
+    );
+  }
+}
