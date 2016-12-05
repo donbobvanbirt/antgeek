5c24124d1d17efc34b62c733398eb049914349f1
Donovan <donovan6925@gmail.com>
2016-12-05 11:16:37 -0500

    added semantic image cards

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import ImageCard from './ImageCard';
+
 const ImageList = (props) => {
   let picList = '';
   if (props.images) {
     picList = props.images.map((pic) => {
       const { url, _id, name } = pic;
       return (
-        <img className="listimg" src={url} alt={name} key={_id} />
+        // <img className="listimg" src={url} alt={name} key={_id} />
+        <ImageCard imageUrl={url} />
       );
     });
   }
