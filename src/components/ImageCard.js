0c5db3267144c3f0125b439e43578427182fdd20
Donovan <donovan6925@gmail.com>
2016-12-12 00:16:11 -0500

    added style and user info to image cards

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -6,20 +6,20 @@ import moment from 'moment';
 export default class ImageCard extends Component {
 
   selectPic(id) {
-    console.log('id:', id);
+    // console.log('id:', id);
     browserHistory.push(`/detail/${id}`);
   }
 
   render() {
-    const { url, _id, title, tags, description, comments, timestamp } = this.props.pic;
-
+    const { url, _id, title, tags, description, comments, timestamp, user } = this.props.pic;
+    console.log('this.props.pic', this.props.pic);
     return (
       <Card onClick={() => this.selectPic(_id)}>
         <Image src={url} />
         <Card.Content>
           <Card.Header>{title}</Card.Header>
-          <Card.Meta>{moment(timestamp).format('MMMM Do YYYY')}</Card.Meta>
-          <Card.Description>{description}</Card.Description>
+          <Card.Meta>{user.name}</Card.Meta>
+          <Card.Description>{moment(timestamp).fromNow()}</Card.Description>
         </Card.Content>
         <Card.Content extra>
           <Icon name="comments" />
